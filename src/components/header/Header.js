9d518cc3532924e5ae02b6e91de033cd12d83ed6
Anuoluwapo Gabriel Oyetibo <54692909+Aoyetibo557@users.noreply.github.com>
2023-12-21 22:02:09 -0500

    fixing header logo from div to href tag

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,9 +6,9 @@ import AI_GOD_Logo from "../../assets/images/aigod_ftp.png";
 const Header = () => {
   return (
     <div className="header">
-      <div>
+      <a href="/" rel="noreferrer">
         <img className="logo_img" src={AI_GOD_Logo} alt="AIGOD Logo" />
-      </div>
+      </a>
       <Navbar />
     </div>
   );
